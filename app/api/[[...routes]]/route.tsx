fa2dc01c13853afa0fa5ff96de49c7a9dc9bf009
PBillingsby <philbillingsby@gmail.com>
2024-05-17 14:59:38 -0400

    refactor for API

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -1,12 +1,30 @@
 /** @jsxImportSource frog/jsx */
-
-import { runCliCommand } from '@/app/services/cli'
 import { saveImage } from '@/app/services/saveImage'
 import { Button, Frog, TextInput } from 'frog'
 import { devtools } from 'frog/dev'
 // import { neynar } from 'frog/hubs'
 import { handle } from 'frog/next'
 import { serveStatic } from 'frog/serve-static'
+import fetch from 'node-fetch';
+
+const fetchWithTimeout = async (url: string, options = {}, timeout = 5000) => {
+  const controller = new AbortController();
+  const { signal } = controller;
+  const fetchPromise = fetch(url, { ...options, signal });
+
+  const timeoutId = setTimeout(() => controller.abort(), timeout);
+
+  try {
+    const response = await fetchPromise;
+    clearTimeout(timeoutId);
+    return response;
+  } catch (error: any) {
+    if (error.name === 'AbortError') {
+      throw new Error('Request timed out');
+    }
+    throw error;
+  }
+};
 
 const app = new Frog({
   assetsPath: '/',
@@ -17,6 +35,7 @@ const app = new Frog({
 
 app.frame('/', async (c) => {
   const { buttonValue, inputText, status } = c
+
   return c.res({
     action: '/submit',
     image: (
@@ -37,28 +56,36 @@ app.frame('/', async (c) => {
           width: '100%',
         }}
       >
-        {status === 'response'
-          ? `Nice choice.${inputText?.toUpperCase()}`
-          :
-          <img src="/frame-placeholder.png" width="100%" height="100%" />
-        }
+        <img src="/frame-placeholder.png" width="100%" height="100%" />
       </div>
     ),
     intents: [
       <TextInput placeholder="Enter Prompt" />,
-      <Button value="generate">Generate</Button>,
+      <Button value="submit">Generate</Button>,
       status === 'response' && <Button.Reset>Reset</Button.Reset>,
     ],
   })
 })
 
 app.frame('/submit', async (c) => {
-  const { inputText, url } = c
-
+  const { inputText, url, status } = c
+  let loading = true;
+  let generatedImageUrl;
   try {
-    const filePath = await runCliCommand({ userInput: inputText });
-    const urlPath = filePath.replace('/tmp/lilypad/data/downloaded-files', '');
-    console.log(filePath, urlPath)
+    const URL = "http://js-cli-wrapper.lilypad.tech"
+    const pk = process.env.PRIVATE_KEY
+    const module = "sdxl-pipeline:v0.9-base-lilypad3"
+
+    const response = await fetchWithTimeout(URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ pk, module, inputText })
+    }, 10000).then((raw: any) => raw.json());
+
+    generatedImageUrl = response.url;
+    loading = false
   } catch (error) {
     console.log(error)
     // c.res.status(500).send('Error processing command: ' + error.message);
@@ -68,11 +95,16 @@ app.frame('/submit', async (c) => {
     action: '/',
     image: (
       <div style={{ color: 'white', display: 'flex', fontSize: 60 }}>
-        <img src="https://ipfs.io/ipfs/QmQqzMTavQgT4f4T5v6PWBp7XNKtoPmC9jvn12WPT3gkSE" />
+        {loading ?
+          <img src={"https://ipfs.io/ipfs/QmQqzMTavQgT4f4T5v6PWBp7XNKtoPmC9jvn12WPT3gkSE"} />
+          :
+          <img src={generatedImageUrl} />
+        }
       </div>
     ),
     intents: [
-      <Button value="save">Save</Button>,
+      status === 'response' && <Button>...generating</Button>,
+      status !== 'response' && <Button value="save">Save</Button>,
       <Button value="reset">Reset</Button>,
     ],
   })
